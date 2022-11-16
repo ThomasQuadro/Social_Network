dec7faf9063d023c04eef3fcd35af6877ed47b2e
Thomas Quadro <91244260+ThomasQuadro@users.noreply.github.com>
2022-11-16 10:01:08 +0100

    ajout class user, variables et func connexion, register

---
+++ src/Models/models.php
@@ -0,0 +1,23 @@
+<?php
+
+class Variables{
+    public string $id_user;
+    public string $email;
+    public string $pseudo;
+    public string $password;
+}
+
+class User{
+
+    public PDO $connexion;
+
+    public function __construct(){
+        $this->connexion = new PDO('mysql:host=localhost;dbname=hardware;charset=utf8', 'root', '');
+    }
+
+    public function register(string $email,string $pseudo, string $password){
+        
+    }
+
+
+}
